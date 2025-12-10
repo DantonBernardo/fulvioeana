3db717064542da901702adf74bcd62c5b473f419
Danton <dantyt337@gmail.com>
2025-12-10 11:01:56 -0300

    new logic to load carousel

--- src/components/Feedback/Feedback.tsx
+++ src/components/Feedback/Feedback.tsx
@@ -59,30 +59,30 @@ export default function Feedback({ index = 0 }){
     speed: 600,
     cssEase: "ease-in-out",
     responsive: [
-    {
-      breakpoint: 500,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        centerPadding: "60px",
-      }
-    },
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        centerPadding: "100px",
-      }
-    },
-    {
-      breakpoint: 1100,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        centerPadding: "50px",
-      }
-    },
+      {
+        breakpoint: 1100,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          centerPadding: "50px",
+        }
+      },
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          centerPadding: "100px",
+        }
+      },
+      {
+        breakpoint: 500,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          centerPadding: "60px",
+        }
+      },
   ]
 
   };
